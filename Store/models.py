e7fbe21981f228ed655285fa6eef0920e06aed49
tiago.efigenio <tiago.efigenio@gmail.com>
2022-07-20 22:23:56 -0300

    ajustes urls

--- Store/models.py
+++ Store/models.py
@@ -5,14 +5,14 @@ from django.db import models
 class Departamento(models.Model):
     nome = models.CharField(max_length=20)
     
-    def _str_(self):
+    def __str__(self):
         return self.nome
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return self.nome
 
 class Produto(models.Model):
@@ -24,5 +24,5 @@ class Produto(models.Model):
     lancamento = models.BooleanField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return self.nome
--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,13 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HTTPResponse('Hello World!')
+
+def teste(request):
+    return HttpResponse('Minha página de teste')
+        
+
