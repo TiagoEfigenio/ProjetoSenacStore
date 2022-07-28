755a3896cce649173a86d15acadcfbfa098ed4f8
tiago.efigenio <tiago.efigenio@gmail.com>
2022-07-27 22:26:24 -0300

    tabelas html

--- Store/urls.py
+++ Store/urls.py
@@ -5,6 +5,9 @@ from Store import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('teste/', views.teste, name='teste')
+    path('teste/', views.teste, name='teste'),
+    path('departamentos/',views.departamentos, name='departamentos'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('produtos/',views.produtos, name='produtos' )
 
 ]
--- Store/views.py
+++ Store/views.py
@@ -2,7 +2,8 @@ from http.client import HTTPResponse
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
-from Store.models import Departamento
+from Store.models import Departamento, Categoria, Produto 
+
 
 # Create your views here.
 
@@ -16,5 +17,19 @@ def teste(request):
     depto = Departamento.objects.all()
     context = {'departamentos':depto}
     return render(request, 'teste.html', context)
-        
+
+def departamentos(request):
+    depto = Departamento.objects.all()
+    context = {'departamentos':depto}
+    return render(request, 'departamentos.html', context)
+
+def categorias(request):
+    categ = Categoria.objects.all()
+    context = {'categorias':categ}
+    return render(request, 'categorias.html', context)           
+
+def produtos(request):
+    prod = Produto.objects.all()
+    context = {'produtos':prod}
+    return render(request, 'produtos.html', context)        
 
